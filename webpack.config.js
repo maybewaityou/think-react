8cd607354a69d60a4530c001f3cd9f3ed44675c4
MeePwn <maybewaityou@gmail.com>
2017-07-10 09:53:55 +0800

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,6 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.html$/,
-                loader: 'file-loader?name=[name].[ext]'
-            },
-            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: [
@@ -37,6 +33,12 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
                     'file-loader'
                 ]
