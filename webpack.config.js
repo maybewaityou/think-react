109ac6d0dea7516c31cbbcb88fc1b39195f4e8b5
MeePwn <maybewaityou@gmail.com>
2017-07-11 04:56:11 +0800

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,9 @@ const webpackConfig = {
                 ],
                 exclude: /node_modules/
             },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
             /* Loading CSS */
             {
                 test: /\.css$/,
@@ -102,7 +105,15 @@ const webpackConfig = {
                     'xml-loader'
                 ]
             }
-        ]
+        ],
+        // When importing a module whose path matches one of the following, just
+        // assume a corresponding global variable exists and use that instead.
+        // This is important because it allows us to avoid bundling all of our
+        // dependencies, which allows browsers to cache those libraries between builds.
+        externals: {
+            'react': 'React',
+            'react-dom': 'ReactDOM'
+        },
     }
 };
 
