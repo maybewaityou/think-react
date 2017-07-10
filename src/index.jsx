b36a4a31b1e9d2919451a5d3f6d773ab4279d5d8
MeePwn <maybewaityou@gmail.com>
2017-07-10 17:26:04 +0800

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@
  */
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import './assets/stylesheet/style.css';
 // import { Provider } from 'react-redux';
 // import configureStore from './dataflow/store/Store';
 // import Just from './main/context/Just';
