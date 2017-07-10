7ad688d8ba3ad82d10065a9b2d270c1ef32145e8
MeePwn <maybewaityou@gmail.com>
2017-07-10 10:38:52 +0800

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.jsx',
-        print: './src/print.js'
+        print: './src/print.js',
+        vendor: ['lodash']
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'think-react'
         })
