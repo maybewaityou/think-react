557fabe6d74458faab7f02d022663d704bf13cae
MeePwn <maybewaityou@gmail.com>
2017-07-10 14:14:40 +0800

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -31,37 +31,3 @@ class ReduxApp extends PureComponent {
 }
 
 ReactDOM.render(( <ReduxApp /> ), document.getElementById('content'));
-
-// import _ from 'lodash';
-// import './style.css';
-// import Icon from './ic_launcher.png';
-// import Data from './data.xml';
-//
-// import printMe from './print';
-//
-// function component() {
-//     var element = document.createElement('div');
-//
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     element.classList.add('hello');
-//
-//     // Add the image to our existing div.
-//     var myIcon = new Image();
-//     myIcon.src = Icon;
-//
-//     element.appendChild(myIcon);
-//
-//     console.log(Data);
-//
-//     var btn = document.createElement('button');
-//     btn.innerHTML = 'Click me and check the console!';
-//     btn.onclick = printMe;
-//     element.appendChild(btn);
-//
-//     var input = document.createElement('input');
-//     element.appendChild(input);
-//
-//     return element;
-// }
-//
-// document.body.appendChild(component());
