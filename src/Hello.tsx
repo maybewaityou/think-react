b7e41dfe531afecbc18a07011a9a280c3b8866e2
MeePwn <maybewaityou@gmail.com>
2017-07-11 09:08:07 +0800

    first commit

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -10,6 +10,27 @@ export interface IHelloProps {
 // State is never set so we use the 'undefined' type.
 export class Hello extends React.Component<IHelloProps, undefined> {
     public render() {
-        return <h1 className="hello">Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return (
+            <div className="hello">
+                <h1>
+                    Hello from {this.props.compiler} and {this.props.framework}!
+                </h1>
+                <div>
+                    name :  <input />
+                </div>
+                <div style={styles.bgColor}>
+                    password :  <input />
+                </div>
+            </div>
+        );
     }
 }
+
+const styles = {
+    bgColor: {
+        backgroundColor: 'blue',
+    },
+    container: {
+        flex: 1,
+    },
+};
