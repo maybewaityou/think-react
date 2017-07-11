33534fa7748a7560b23722a6ccc8271acd8d4543
MeePwn <maybewaityou@gmail.com>
2017-07-11 09:00:23 +0800

    first commit

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,12 +1,15 @@
-import * as React from "react";
+import * as React from 'react';
 import './assets/stylesheet/style.css';
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface IHelloProps {
+    compiler: string;
+    framework: string;
+}
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
-export class Hello extends React.Component<HelloProps, undefined> {
-    render() {
-        return <h1 className='hello'>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+export class Hello extends React.Component<IHelloProps, undefined> {
+    public render() {
+        return <h1 className="hello">Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
