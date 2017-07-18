aa404168473a9131644d68ef427306b4cf1bfd2b
MeePwn <maybewaityou@gmail.com>
2017-07-18 09:12:45 +0800

    first commit

--- src/demos/redux-observable/functional/Functions.ts
+++ src/demos/redux-observable/functional/Functions.ts
@@ -5,5 +5,22 @@
  * description:
  *
  */
+import curry from 'lodash/curry';
 
 export const add = (x: number) => (y: number) => x + y;
+
+export const match = curry((what: any, str: string) => {
+    return str.match(what);
+});
+
+export const replace = curry((what: any, replacement: string, str: string) => {
+    return str.replace(what, replacement);
+});
+
+export const filter = curry((f: any, array: any[]) => {
+    return array.filter(f);
+});
+
+export const map = curry((f: any, array: any[]) => {
+    return array.map(f);
+});
