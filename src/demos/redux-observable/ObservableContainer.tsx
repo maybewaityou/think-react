fbefa32049489981c3645d894ffefa4237c00498
MeePwn <maybewaityou@gmail.com>
2017-07-18 09:55:29 +0800

    first commit

--- src/demos/redux-observable/ObservableContainer.tsx
+++ src/demos/redux-observable/ObservableContainer.tsx
@@ -13,7 +13,7 @@ import actionCreator from '../../dataflow/actions/ActionCreator';
 import { connection } from '../../dataflow/connect/connection';
 import { log } from '../../main/utilities/debug/DebugUtility';
 import Container from './functional/Container';
-import { add } from './functional/Functions';
+import { add, compose, filter, map, match, reduce, replace } from './functional/Functions';
 import Maybe from './functional/Maybe';
 import ObservableView, { ObservableStatelessView } from './ObservableView';
 import { $getError, $getHomeData, $getHomeDataSelector, isSuccess } from './selector/Selectors';
@@ -66,9 +66,38 @@ export default class ObservableContainer extends React.PureComponent<IObservable
         //     return Maybe.of(2).map((a: any) => a + 3);
         // });
         // console.log(result);
-        const result = Maybe.of(2).map(add).apply(Maybe.of(3));
+        // const result = Maybe.of(2).map(add).apply(Maybe.of(3));
+        // console.log(result);
+        // const result = match(/\s+/g)('hello world');
+        // console.log(result);
+        //
+        // const hasSpaces = match(/\s+/g);
+        // const result = hasSpaces('Welcome to China ~');
+        // console.log(result);
+        // const result = filter(hasSpaces)(['tori_spelling', 'tori amos']);
+        //
+        // const findSpaces = filter(hasSpaces);
+        // const result = findSpaces(['tori_spelling', 'tori amos']);
+        //
+        // const noVowels = replace(/[aeiou]/ig);
+        // const censored = noVowels('*');
+        // const result = censored('Welcome to China ~');
+        // console.log(result);
+        //
+
+        // const toUpperCase = (x: string) => x.toUpperCase();
+        // const exclaim = (x: string) => x + '!';
+        // const shout = compose(exclaim, toUpperCase);
+        // const result = shout('send in the clowms');
+        // console.log(result);
+
+        const reverse = reduce((acc: any, x: any) => [x].concat(acc), []);
+        const head = (x: any[]) => x[0];
+        const last = compose(head, reverse);
+        const result = last(['jumpkick', 'roundhouse', 'uppercut']);
         console.log(result);
-    }
+
+        }
 
     public render() {
         const { success, $data, $error } = this.props;
--- src/demos/redux-observable/functional/Functions.ts
+++ src/demos/redux-observable/functional/Functions.ts
@@ -5,22 +5,19 @@
  * description:
  *
  */
-import curry from 'lodash/curry';
+// import curry from 'lodash/curry';
 
 export const add = (x: number) => (y: number) => x + y;
 
-export const match = curry((what: any, str: string) => {
-    return str.match(what);
-});
+export const match = (what: any) => (str: string) => str.match(what);
 
-export const replace = curry((what: any, replacement: string, str: string) => {
-    return str.replace(what, replacement);
-});
+export const replace = (what: any) => (replacement: string) => (str: string) =>
+    str.replace(what, replacement);
 
-export const filter = curry((f: any, array: any[]) => {
-    return array.filter(f);
-});
+export const filter = (f: any) => (array: any []) => array.filter(f);
 
-export const map = curry((f: any, array: any[]) => {
-    return array.map(f);
-});
+export const map = (f: any) => (array: any[]) => array.map(f);
+
+export const compose = (f: any, g: any) => (x: any) => f(g(x));
+
+export const reduce = (f: any, x: any) => (array: any[]) => array.reduce(f, x);
