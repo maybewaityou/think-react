0b1c32dc39d9f0864840f64a84fccd8acfd19de7
MeePwn <maybewaityou@gmail.com>
2017-07-21 17:10:11 +0800

    first commit

--- src/demos/relay/RelayContainer.tsx
+++ src/demos/relay/RelayContainer.tsx
@@ -7,6 +7,7 @@
  */
 import { Map } from 'immutable';
 import * as React from 'react';
+import Relay from 'react-relay';
 import { PureComponent } from '../../main/components/high-order-component/Decorator';
 import { log } from '../../main/utilities/debug/DebugUtility';
 import RelayView from './RelayView';
@@ -27,7 +28,7 @@ export default class RelayContainer extends PureComponent<IRelayContainerProps, 
     }
 
     public handleTestClick = () => {
-        log('======');
+        log('====');
     }
 
     public render() {
