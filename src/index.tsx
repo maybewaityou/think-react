c5fc2cf3adc1edb428737be0b6bbe91c28ec78ed
MeePwn <maybewaityou@gmail.com>
2017-07-24 04:07:42 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import whyDidYouUpdate from 'why-did-you-update';
 import './assets/stylesheet/style.css';
 import store from './dataflow/store/Store';
 import FunctionalContainer from './demos/functional/FunctionalContainer';
 import ApolloContainer from './demos/react-apollo/ApolloContainer';
 import ObservableContainer from './demos/redux-observable/ObservableContainer';
-
-// whyDidYouUpdate(React);
 
 ReactDOM.render((
     <Provider store={store}>
