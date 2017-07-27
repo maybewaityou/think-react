e42895b00e744a84fdc940daeecc568ad856f025
MeePwn <maybewaityou@gmail.com>
2017-07-27 12:44:53 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@
  *
  */
 import * as React from 'react';
-// import { ApolloClient, ApolloProvider } from 'react-apollo';
+import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './assets/stylesheet/style.css';
@@ -17,17 +17,20 @@ import RecomposeContainer from './demos/recompose/RecomposeContainer';
 import ObservableContainer from './demos/redux-observable/ObservableContainer';
 import StreamContainer from './demos/stream/StreamContainer';
 
-const App = (
-    <Provider store={store}>
-        <RecomposeContainer />
-    </Provider>
-);
-
-// const client = new ApolloClient();
-// const ApolloApp = (
-//     <ApolloProvider client={client}>
-//         <ApolloContainer />
-//     </ApolloProvider>
+// const App = (
+//     <Provider store={store}>
+//         <RecomposeContainer />
+//     </Provider>
 // );
 
-ReactDOM.render(App, document.getElementById('app'));
+const client = new ApolloClient({
+    networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj5llp3l43vdl0122udwc6cma'}),
+});
+
+const ApolloApp = (
+    <ApolloProvider client={client}>
+        <ApolloContainer />
+    </ApolloProvider>
+);
+
+ReactDOM.render(ApolloApp, document.getElementById('app'));
