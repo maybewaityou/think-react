1521cf7982655c96f6d25f85258ed096ccc21911
MeePwn <maybewaityou@gmail.com>
2017-08-04 08:36:20 +0800

    first commit

--- src/demos/react-apollo/graphql/graphqlDSL.ts
+++ src/demos/react-apollo/graphql/graphqlDSL.ts
@@ -8,50 +8,50 @@
 import { gql } from '../../../main/third-party/transform/graphQL';
 
 export const QUERY_USERS_AND_POSTS = gql`
-    query {
-        users {
-            id
-            firstName
-            lastName
-        }
-        posts {
-            id
-            post
-        }
+  query {
+    users {
+      id
+      firstName
+      lastName
     }
+    posts {
+      id
+      post
+    }
+  }
 `;
 
 export const CREATE_USER = gql`
-    mutation($firstName: String!, $lastName: String!) {
-        createUser(firstName: $firstName, lastName: $lastName) {
-            id
-            firstName
-            lastName
-        }
+  mutation($firstName: String!, $lastName: String!) {
+    createUser(firstName: $firstName, lastName: $lastName) {
+      id
+      firstName
+      lastName
     }
+  }
 `;
 
 export const CREATE_POST = gql`
-    mutation($data: String!) {
-        createPost(data: $data) {
-            id
-            post
-        }
+  mutation($data: String!) {
+    createPost(data: $data) {
+      id
+      post
     }
+  }
 `;
 
 export const RESET_USERS_AND_POSTS = gql`
-    mutation {
-        reset {
-            users {
-                id
-                firstName
-                lastName
-            }
-            posts {
-                id
-                post
-            }
-        }
+  mutation {
+    reset {
+      users {
+        id
+        firstName
+        lastName
+      }
+      posts {
+        id
+        post
+      }
     }
+  }
 `;
