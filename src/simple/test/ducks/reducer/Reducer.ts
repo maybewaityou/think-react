2660993c91fc7db88e74c08ce66c70446433703d
MeePwn <maybewaityou@gmail.com>
2017-09-13 15:50:04 +0800

    first commit

--- src/simple/test/ducks/reducer/Reducer.ts
+++ src/simple/test/ducks/reducer/Reducer.ts
@@ -8,7 +8,7 @@
 import Immutable, { fromJS, Map } from 'immutable';
 import { acceptActions, IAction } from 'mario-ducks';
 import { log } from 'mario-utilities';
-import { TEST_ACTION, TEST_ACTION_UNACCEPT } from '../actions/index';
+import * as Actions from '../actions/index';
 
 const $initialState: Map<string, any> = fromJS({
   test00: {},
@@ -17,15 +17,15 @@ const $initialState: Map<string, any> = fromJS({
 
 export default acceptActions((state: Map<string, any> = $initialState, action: IAction) => {
   switch (action.type) {
-    case TEST_ACTION:
+    case Actions.TEST_ACTION:
       log('accept');
       return state.set('test00', fromJS(action.payload));
-    case TEST_ACTION_UNACCEPT:
+    case Actions.TEST_ACTION_UNACCEPT:
       log('unaccept');
       return state.set('test01', fromJS(action.payload));
     default:
       return state;
   }
 }, [
-  TEST_ACTION,
+  Actions,
 ]);
