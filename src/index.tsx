39eb2bc95bda7880c406ad9b16dd7460a043ede9
MeePwn <maybewaityou@gmail.com>
2017-09-14 16:48:53 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -42,7 +42,7 @@ const store = configureApolloStore({}, apolloClient, networkClient, rootReducer,
 
 const ApolloApp = (
   <MarioProvider client={apolloClient} store={store}>
-    <TestContainer />
+    <ObservableContainer />
   </MarioProvider>
 );
 
