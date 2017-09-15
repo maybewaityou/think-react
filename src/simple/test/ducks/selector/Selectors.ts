6e082d8f7c42d8ddbdcc0355015c82d68d8fd1f3
MeePwn <maybewaityou@gmail.com>
2017-09-15 20:57:23 +0800

    first commit

--- src/simple/test/ducks/selector/Selectors.ts
+++ src/simple/test/ducks/selector/Selectors.ts
@@ -11,7 +11,7 @@ import { log, toString } from 'mario-utilities';
 
 const test00Selector = createSelector([
   (state: any) => state.testReducer.get('test00'),
-], (result: any) => result);
+], (test00Result: any) => test00Result);
 
 export default (state: any) => ({
   model: test00Selector(state),
